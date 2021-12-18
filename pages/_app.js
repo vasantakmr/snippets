3f2ecba2330cbe4ee45b70703427286adf47a07d
Vasanta Kumar <vasantakmr6@gmail.com>
2021-12-19 00:52:30 +0530

    name was causing build error. updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import { Toaster } from 'react-hot-toast'
-import Navbar from '@components/navbar'
+import Navbar from '@components/Navbar'
 
 function MyApp({ Component, pageProps }) {
   return (
