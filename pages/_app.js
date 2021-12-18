69c5b54df78bd96c4a869d93729a26675bbf6dca
Vasanta Kumar <vasantakmr6@gmail.com>
2021-12-19 00:12:33 +0530

    Added Navbar along with Empty Pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import '../styles/globals.css'
 import { Toaster } from 'react-hot-toast'
+import Navbar from '../components/navbar'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Navbar />
       <Component {...pageProps} />
       <Toaster
         position="bottom-right"
---
+++ pages/components.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Components() {
+  return (
+    <>
+      {/* Keep the existing code here */}
+      <div className="container">
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main>
+            <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                Work in progress.
+            </div>
+        </main>
+      </div>
+      
+    </>
+  )
+}
